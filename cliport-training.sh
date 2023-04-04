4f0af9a7ac04e41d748c3cffd9235bfbab5902eb
DennisLent <dennis.lentschig955@gmail.com>
2023-04-04 14:28:49 +0200

    update to requirements and shell script

--- cliport-training.sh
+++ cliport-training.sh
@@ -12,6 +12,16 @@ module load 2022r2
 module load python/3.8
 source cliport_env/bin/activate
 
-srun <name_of_module> 
+
+srun python cliport/train.py train.task=towers-of-hanoi-seq-seen-colors \
+                        train.agent=simple_cliport \
+                        train.attn_stream_fusion_type=add \
+                        train.trans_stream_fusion_type=conv \
+                        train.lang_fusion_type=mult \
+                        train.n_demos=10 \
+                        train.n_val=10 \
+                        train.n_steps=1000 \
+                        train.exp_folder=exps \
+                        dataset.cache=False
 
 
