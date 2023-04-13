fd8f8e84624dd3d82a3402445f5eb09aeb9dfbc5
Antonio López Rivera <antonlopezr99@gmail.com>
2023-04-13 15:27:57 +0200

    Reproduction scripts updated

--- reproduction_scripts/eval_simple.sh
+++
@@ -1,9 +0,0 @@
-export CLIPORT_ROOT=$(pwd)
-# Evaluate simplified model on towers of hanoi problem
-python cliport/eval.py eval_task=towers-of-hanoi-seq-seen-colors \
-                       agent=simple_cliport \
-                       mode=val \
-                       n_demos=10 \
-                       train_demos=10 \
-                       checkpoint_type=val_missing \
-                       exp_folder=exps
---
+++ reproduction_scripts/reproduce_unseen_colors_simple.sh
@@ -0,0 +1,37 @@
+export CLIPORT_ROOT=$(pwd)
+
+N_DEMOS=${1:-1} # Script argument, defaulting to 1
+N_EPOCHS=10000
+
+echo "============================================"
+echo "    Starting training and evaluation of"
+echo "            SIMPLE CLIPORT MODEL"
+echo "                     for"
+echo "    ~ towers-of-hanoi-seq-seen-colors ~"
+echo "                    with:"
+echo "               n_demos  = ${N_DEMOS}"
+echo "               n_epochs = ${N_EPOCHS}"
+echo "============================================"
+
+sleep 1
+
+# Train simple model on towers of hanoi problem
+python cliport/train.py train.task=towers-of-hanoi-seq-unseen-colors \
+                        train.agent=simple_cliport \
+                        train.attn_stream_fusion_type=add \
+                        train.trans_stream_fusion_type=conv \
+                        train.lang_fusion_type=mult \
+                        train.n_demos=${N_DEMOS} \
+                        train.n_val=${N_DEMOS} \
+                        train.n_steps=${N_EPOCHS} \
+                        train.exp_folder=exps \
+                        dataset.cache=False | tee -a reproduction_logs/cliport_simple_unseen_train_n${N_DEMOS}.txt
+
+# Evaluate simple model on towers of hanoi problem
+python cliport/eval.py eval_task=towers-of-hanoi-seq-unseen-colors \
+                       agent=simple_cliport \
+                       mode=val \
+                       n_demos=10 \
+                       train_demos=${N_DEMOS} \
+                       checkpoint_type=val_missing \
+                       exp_folder=exps | tee -a reproduction_logs/cliport_simple_unseen_eval_n$N_DEMOS.txt
